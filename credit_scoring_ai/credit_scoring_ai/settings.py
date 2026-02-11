ba98c1548177daf0af001d6a4776962f65b0c146
Yash Sharma <yasharma2324@gmail.com>
2026-02-12 00:37:39 +0530

    first commit

--- credit_scoring_ai/credit_scoring_ai/settings.py
+++ credit_scoring_ai/credit_scoring_ai/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4mn!zcxs#fa26yu1x3ze3e_4q!_wwinb-q#^q(9o8#jgp8+fcy"
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
